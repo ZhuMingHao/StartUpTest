cc2e1c775e6eecf1c0c568efbd9d5658fdba333d
Nico Zhu <v-zhumin@microsoft.com>
2018-04-16 13:05:20 +0800

    Add project files.

---
+++ StartUpTest/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StartUpTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string payload = e.Parameter as string;
+            if (!string.IsNullOrEmpty(payload))
+            {
+                //activationText.Text = payload;
+
+                //if (payload == "StartupTask")
+                //{
+                //    requestButton.IsEnabled = false;
+                //    requestResult.Text = "Enabled";
+                //    SolidColorBrush brush = new SolidColorBrush(Colors.Gray);
+                //    requestResult.Foreground = brush;
+                //    requestPrompt.Foreground = brush;
+                //}
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StartupTask startupTask = await StartupTask.GetAsync("MyStartupId");
+            switch (startupTask.State)
+            {
+                case StartupTaskState.Disabled:
+                    // Task is disabled but can be enabled.
+                    StartupTaskState newState = await startupTask.RequestEnableAsync();
+                    Debug.WriteLine("Request to enable startup, result = {0}", newState);
+                    break;
+
+                case StartupTaskState.DisabledByUser:
+                    // Task is disabled and user must enable it manually.
+                    MessageDialog dialog = new MessageDialog(
+                        "I know you don't want this app to run " +
+                        "as soon as you sign in, but if you change your mind, " +
+                        "you can enable this in the Startup tab in Task Manager.",
+                        "TestStartup");
+                    await dialog.ShowAsync();
+                    break;
+
+                case StartupTaskState.DisabledByPolicy:
+                    Debug.WriteLine(
+                        "Startup disabled by group policy, or not supported on this device");
+                    break;
+
+                case StartupTaskState.Enabled:
+                    Debug.WriteLine("Startup is enabled.");
+                    break;
+            }
+        }
+    }
+}
